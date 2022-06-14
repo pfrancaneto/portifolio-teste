ee0da8c35e3f3f8ac86031a8873d168cef1c1306
Pedro França <pfranca.neto@gmail.com>
2022-06-13 23:00:30 -0300

    criando Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Pedro França</h1>
+        <h5 className="text-light">Frontend Development</h5>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
