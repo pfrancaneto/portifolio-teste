08d126faa4e8117e77bbf03f6da949047bf62fba
Pedro França <pfranca.neto@gmail.com>
2022-06-14 17:33:59 -0300

    componente header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./header.css";
+import ME from '../../assets/me.png';
+
+import CTA from './Cta/CTA';
+import HeaderSocials from "./HeaderSocials/HeaderSocials";
 
 const Header = () => {
   return (
@@ -8,6 +12,12 @@ const Header = () => {
         <h5>Hello, I'm</h5>
         <h1>Pedro França</h1>
         <h5 className="text-light">Frontend Development</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   );
