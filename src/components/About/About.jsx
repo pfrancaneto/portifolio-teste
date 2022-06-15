bb6f12fd4d9573c6d50a50f174f49347ad749c04
Pedro França <pfranca.neto@gmail.com>
2022-06-15 19:46:08 -0300

    about jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './about.css';
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>50+ Completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore totam ratione est beatae maiores molestiae nemo incidunt eligendi non doloribus quis voluptatem inventore amet nostrum, perspiciatis sint a culpa dignissimos?</p>
+          <a href="#contact" className='btn btn-primary'>Let's a Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
